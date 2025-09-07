664eebb046f5a72c9b1661bec47bb51882a8cc76
mysterious-diamond <aaronkenn10@gmail.com>
2025-09-07 11:58:05 +0700

    Added placeholder login page

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, session
 import mysql.connector
 
 app = Flask(__name__)
+app.secret_key = "123"
 conn = mysql.connector.connect(
     host = "172.29.208.1",
     user = "root",
@@ -11,10 +12,30 @@ conn = mysql.connector.connect(
 
 @app.route("/")
 def home() :
-    cursor = conn.cursor()
-    cursor.execute("SELECT id FROM users WHERE username = 'Aaron'")
-    results = cursor.fetchone()
-    return render_template("login.html", res=results)
+    if "username" not in session :
+        return redirect("/login")
+    
+    return render_template("home.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login() :
+    if request.method == "GET" :
+        return render_template("login.html")
+    
+    cur = conn.cursor()
+    
+    username = request.form.get("username")
+    passwd = request.form.get("password")
+    
+    cur.execute("SELECT id, username, passwd FROM users WHERE username = %s", (username,))
+    res = cur.fetchone()
+    
+    if res and passwd == res["passwd"]:
+        session["username"] = username
+        session["id"] = res["id"]
+        return redirect("/")
+        
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
